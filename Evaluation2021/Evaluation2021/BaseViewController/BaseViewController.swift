885b3de8d031b73822d5c023bc3a9b396b699bc5
sanat.salian <sanat.salian@robosoftin.com>
2021-02-13 09:39:08 +0530

    Added Soryboard

---
+++ Evaluation2021/Evaluation2021/BaseViewController/BaseViewController.swift
@@ -0,0 +1,41 @@
+//
+//  BaseViewController.swift
+//  Evaluation2021
+//
+//  Created by Sanat Salian on 13/02/21.
+//  Copyright © 2021 Sanat Salian. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+    
+    var navigationView: UIView? {
+        return UIView()
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    func setupNavigatinView() {
+        if let navView = navigationView {
+            
+            view.addSubview(navView)
+            navView.translatesAutoresizingMaskIntoConstraints = false
+            navView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0.0).isActive = true
+            navView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0.0).isActive = true
+            navView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0.0).isActive = true
+            navView.heightAnchor.constraint(equalToConstant: 94).isActive = true
+            view.layoutIfNeeded()
+            
+        }
+    }
+    
+    func displayError(with message: String) {
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    
+}
